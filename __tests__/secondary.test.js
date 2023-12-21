388baede1f58547463a060cecd73ee41d2b020bc
ESC18 <ponderosaprintingus@gmail.com>
2023-12-21 10:27:09 -0800

    Finish tests and create logic

--- __tests__/secondary.test.js
+++ __tests__/secondary.test.js
@@ -15,3 +15,27 @@ describe("Blank", () => {
     });
 })
 */
+
+import { dayCalc, timePass } from "../src/js/secondary";'../src/js/secondary.js'
+
+describe("dayCalc", () => {
+    test("This function should return the day of the week based off of a date", () => {
+        expect(dayCalc('2023-12-19')).toEqual('Monday');
+        expect(dayCalc('2023-12-20')).toEqual('Tuesday');
+        expect(dayCalc('2023-12-21')).toEqual('Wednesday');
+        expect(dayCalc('2023-12-22')).toEqual('Thursday');
+        expect(dayCalc('2023-12-23')).toEqual('Friday');
+        expect(dayCalc('2023-12-24')).toEqual('Saturday');
+        expect(dayCalc('2023-12-25')).toEqual('Sunday');
+    })
+})
+
+describe("timePass", () => {
+    test("should return a string with hours, minutes, and seconds since the provided date", () => {
+        const result = timePass('2023-12-19');
+        const [hrDif, minDif, secDif] = result.split('/').map(Number);
+        expect(hrDif).toBeGreaterThanOrEqual(0); 
+        expect(minDif).toBeGreaterThanOrEqual(0); 
+        expect(secDif).toBeGreaterThanOrEqual(0); 
+    });
+});
--- src/js/secondary.js
+++ src/js/secondary.js
@@ -13,3 +13,29 @@ export default class Blank {
         return this.here * this.there;
     }
 */
+ 
+export function dayCalc(dateStr) {
+    let date = new Date(dateStr);
+    let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let dayOfWeek = weekdays[date.getDay()];
+    return dayOfWeek;
+}
+
+export function timePass(dateStr) {
+    let dateStr2 = new Date(dateStr);
+    let timeDif =  new Date() - dateStr2;
+    let secDif = Math.floor(timeDif / 1000);
+    let minDif = Math.floor(secDif / 60);
+    let hrDif = Math.floor(minDif / 60);
+    return `${hrDif}/${minDif}/${secDif}`;
+}
+
+function displayDate() {
+    let input = document.getElementById("dateInput").value;
+    let resultDiv = document.getElementById("resultDiv");
+    resultDiv.innerHTML = `The day of the week for <span style="color:lightBlue">${input}</span> is <span style="color:Blue">${dayCalc(input)}</span>. <br> The amount of <span style="color:orange">Hours/Minutes/Seconds</span> since specified date are: <span style="color:Red">${timePass(input)}</span>`
+}
+
+export function initialization() {
+    document.getElementById("button").addEventListener("click", displayDate);
+}
